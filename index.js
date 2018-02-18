80ac2667a0c3a256bff6563f6a269a952acec65a
vision23media <aedwards@iadv-aedwards-mac.fios-router.home>
2018-02-18 03:14:44 -0500

    404

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 function v23m(res){
-	//res.send('hello world');
+	res.send('hello world');
   	console.log("VISION23MEDIA");
-  	res.render('index');
+  	//res.render('index');
 }
 
-app.get('/index.html', (req, res) => v23m(res))
+app.get('/vision', (req, res) => v23m(res))
+
+app.get('/', (req, res) => res.status(404).sendFile('/public/404.jpg'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
