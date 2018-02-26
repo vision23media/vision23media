e52d65030f4b527740ed1658e24174aca0a9536c
vision23media <aedwards@iadv-aedwards-mac.fios-router.home>
2018-02-26 14:41:36 -0500

    New files

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+const blogEngine = require('./blog')
 const app = express()
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+var partialsDir = __dirname + '/../views/partials';
+var filenames = fs.readdirSync(partialsDir);
+
+console.log(filenames);
+
 
 function v23m(res){
 	res.send('hello world');
@@ -7,8 +19,15 @@ function v23m(res){
   	//res.render('index');
 }
 
-app.get('/vision', (req, res) => v23m(res))
+hbs.registerPartial('myPartial', 'metoo');
 
-app.get('/', (req, res) => res.status(404).sendFile('/public/404.jpg'))
+app.get('/vision', (req, res) => v23m(res))
+app.get('/about', (req, res) =>  res.render('about'))
+
+//app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+
+app.get('/', function(req, res) {
+    res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
